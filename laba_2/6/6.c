54b4b5d8add8124841a19383df05da50b624c5be
Julia <kon1976y@gmail.com>
2022-10-12 18:19:08 +0300

    laba2 task 6

---
+++ laba_2/6/6.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int* substring_search(int count, char* s, ...) {
+    FILE* fIn = NULL;
+    int k, n, symb = 0, el = 0;
+    char c;
+    int* count_ = (int*)calloc(count, sizeof(int));
+    if (count_ == NULL) {
+        return count_;
+    }
+    va_list file;
+    va_start(file, s);
+    for (int i = 0; i < count; i++) {
+        k = 0;
+        n = 0;
+        fIn = fopen(va_arg(file, char*), "r");
+        if (fIn == NULL) {
+            for (int j = 0; j < count; j++) count_[j] = -1;
+            return count_;
+        }
+
+        while (!feof(fIn)) {
+            c = fgetc(fIn);
+            if (c != '\n' && c!= '\t' && c != EOF && c != ' ') {
+                k++;
+            }
+            if (s[symb] == c) {
+                n++;
+                symb++;
+            }else if (s[symb] != c) {
+                n = 0;
+                symb = 0;
+            }
+            if (n == strlen(s)) {
+                count_[el] = k - n + 1;
+                el++;
+                break;
+            }
+        }
+        fclose(fIn);
+    }
+    va_end(file);
+    return count_;
+}
+
+int main() {
+    char* s = "aranara";
+    int count = 3; 
+    int* res = substring_search(count, s, (char*)"txt1", (char*)"txt2", (char*)"txt3");
+    if (res == NULL) {
+        printf("Память не выделилась!\n");
+        return 1;
+    }
+    for (int i = 0; i < count; i++) {
+        if (res[i] == -1) {
+            printf("Файл не открылся\n");
+            return 2;
+        }
+        if (res[i] == 0) {
+            printf("В %d файле нет нужной подстроки\n", i+1);
+        }
+        else{
+            printf("В %d файле первое вхождение подстроки произошло на %d символе\n", (i+1), res[i]);
+        }
+    }
+    free(res);
+    return 0;
+}
