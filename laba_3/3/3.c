ad92692b152458f07654350000e2b4badae1aa33
Julia <kon1976y@gmail.com>
2022-11-05 13:44:20 +0300

    laba 3 task 2

---
+++ laba_3/3/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define success 0
+#define invalid_input -1
+#define memory_allocation_error -2
+#define error -3
+
+int main(int argc, char* argv[]) {
+    
+}
