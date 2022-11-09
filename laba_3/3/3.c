e0dfbd5fd1049fea543b51b925df5edabd71cfef
Julia <kon1976y@gmail.com>
2022-11-09 16:26:15 +0300

    fix

--- laba_3/3/3.c
+++ laba_3/3/3.c
@@ -5,6 +5,30 @@
 #define memory_allocation_error -2
 #define error -3
 
+typedef struct employee{
+    unsigned int id;
+    char* name;
+    char* surname;
+    double salary;
+}employee;
+
+int from_file_to_array(FILE* fin, employee* arr){
+
+}
+
+
+
 int main(int argc, char* argv[]) {
-    
+    if (argc != 4) {
+        printf("Usage: ./program.exe input_file output_file -(or /)flag(a, d)\n");
+        return error;
+    }
+    if ((strcmp(argv[3], "-a") != 0) && (strcmp(argv[3], "/a") != 0) && (strcmp(argv[3], "-d") != 0) && (strcmp(argv[3], "/d") != 0)) {
+        printf("The flag is not recognized\n");
+        return error;
+    }
+    int k;
+    if ((strcmp(argv[3], "-a") == 0) || (strcmp(argv[3], "/a") == 0)) k = 1;
+    if ((strcmp(argv[3], "-d") == 0) || (strcmp(argv[3], "/d") == 0)) k = 2;
+
 }
